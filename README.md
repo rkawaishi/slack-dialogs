84a0655ce0c937be2310fc15c72ee7856fdf06d4
rk <kantannano@gmail.com>
2019-10-09 01:29:35 +0900

    dynamodb-localの利用テスト

--- README.md
+++ README.md
@@ -77,8 +77,8 @@ Test a single function by invoking it directly with a test event. An event is a 
 Run functions locally and invoke them with the `sam local invoke` command.
 
 ```bash
-slack-dialogs$ docker-compose up
-slack-dialogs$ sam local invoke HelloWorldFunction --event events/event.json --docker-network lambda-local
+slack-dialogs$ docker-compose up -d
+slack-dialogs$ sam local invoke HelloWorldFunction --event events/event.json --env-vars test/env.json
 ```
 
 The SAM CLI can also emulate your application's API. Use the `sam local start-api` to run the API locally on port 3000.
--- hello_world/app.py
+++ hello_world/app.py
@@ -1,11 +1,16 @@
 import json
 import os
+import boto3
+from boto3.dynamodb.conditions import Key, Attr
 
 # import requests
 from jinja2 import Environment, FileSystemLoader
 
 
 env = Environment(loader=FileSystemLoader(os.path.join(os.path.dirname(__file__), "templates"), encoding="utf8"))
+
+DYNAMODB_ENDPOINT = os.getenv("DYNAMODB_ENDPOINT")
+TABLE_NAME = os.getenv("TABLE_NAME")
 
 
 def render_index():
@@ -24,8 +29,18 @@ def render_index():
 
 
 def lambda_handler(event, context):
+    print(DYNAMODB_ENDPOINT)
+    print(TABLE_NAME)
 
-    return render_index()
+    dynamodb = boto3.resource("dynamodb", endpoint_url=DYNAMODB_ENDPOINT)
+    table = dynamodb.Table(TABLE_NAME)
+
+    res = table.query(KeyConditionExpression=Key("name").eq("sasaki-daisuke"))
+    items = res["Items"]
+
+    print(items)
+
+    return items
     # return {
     #     "statusCode": 200,
     #     "body": json.dumps({
